015283e2fdfb5840717a85db0df7ab50f4f974dc
Ahmet ARDIÇ <62844389+AhmetArdic@users.noreply.github.com>
2022-05-02 10:03:13 +0300

    bot active

--- Application/src/app.js
+++ Application/src/app.js
@@ -1,1 +1,10 @@
-import Discord from "discord.js";
+import { Client } from "discord.js";
+
+const client = new Client({
+  intents: ["GUILDS"], //istedigimiz instentsler icin array kullanabiliriz
+});
+client.login("OTcwNTcyNzM5MzM1MjkwODgw.Ym96TQ.ev7ZFVXYOn0weMXMMsvs_K5BTpE");
+
+client.on("ready", () => {
+  console.log("bot hazır!!");
+});
