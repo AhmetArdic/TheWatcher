079f6717c6a988f38cbdf1cd266eb1ccf815d19c
Ahmet ARDIÇ <62844389+AhmetArdic@users.noreply.github.com>
2022-05-02 10:33:23 +0300

    dotenv ile config ve komik geliştirmeler

--- Application/src/app.js
+++ Application/src/app.js
@@ -1,10 +1,29 @@
 import { Client } from "discord.js";
+import "dotenv/config";
 
 const client = new Client({
-  intents: ["GUILDS"], //istedigimiz instentsler icin array kullanabiliriz
+  intents: ["GUILDS"], //*istedigimiz instentsler icin array kullanabiliriz
+  presence: {
+    status: "dnd",
+    //activities: [{ name: "müconun deliğiyle", type: "PLAYING" }],
+  },
 });
-client.login("OTcwNTcyNzM5MzM1MjkwODgw.Ym96TQ.ev7ZFVXYOn0weMXMMsvs_K5BTpE");
 
-client.on("ready", () => {
+client.login(process.env.token);
+
+client.addListener("ready", () => {
+  // veya client.on() şeklinde de kullanılabilir
+  /* bazi islemler client hazir olunca yapilabilir
+  örnegin presence degistirme */
+
   console.log("bot hazır!!");
+
+  const presenceList = [
+    { name: "müconun deliğiyle", type: "PLAYING" },
+    { name: "müconun deliğini", type: "WATCHING" },
+  ];
+  setInterval(() => {
+    const randomPresence = Math.floor(Math.random() * presenceList.length);
+    client.user.setPresence({ activities: [presenceList[randomPresence]] });
+  }, 5800);
 });
